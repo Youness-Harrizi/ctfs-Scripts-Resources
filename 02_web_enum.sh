b38a821248b9372060b4c2b8575a2258a97743ea
Youness-Harrizi <younessharrizi4@gmail.com>
2023-02-22 09:16:52 +0100

    modify web script

--- 02_web_enum.sh
+++ 02_web_enum.sh
@@ -2,9 +2,9 @@
 mkdir web 
 dir=$(pwd)
 wordlist="/usr/share/SecLists/Discovery/Web-Content/raft-medium-directories.txt"
-echo "=======start====="
-dirsearch.py -u $1 -w $wordlist -e asp,aspx,php,php,html,txt,bak -o $dir/web/dirsearch.log -r 
-echo "=======finished====="
+echo "=======start whatweb====="
 whatweb -a 3 $1 |tee web/whatweb.log
-echo "=======finished====="
+echo "=======start nuclei ====="
 nuclei -u $1 |tee web/nuclei.log
+echo "=======start dirsearch====="
+dirsearch.py -u $1 -w $wordlist -e html,txt,bak -o $dir/web/dirsearch.log  -t 16
