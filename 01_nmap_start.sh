879addb7bb110ec21e0f8d00c4b32d93cf600930
Youness-Harrizi <younessharrizi4@gmail.com>
2023-02-22 09:14:24 +0100

    add --fast option

--- 01_nmap_start.sh
+++ 01_nmap_start.sh
@@ -1,7 +1,38 @@
 #! /bin/bash
-#nmap
-mkdir nmap
-nmap -p- -oN nmap/all_ports $1 -vvv
-export ports=$(cat nmap/all_ports |grep "open"|cut -d "/" -f1|tr "\n" ","| sed 's/.$//')
-echo $ports
-nmap -p$ports -sC -sV $1 -oN nmap/services -vvv
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -f|--fast)
+            fast=true
+            shift
+            ;;
+        *)
+            target="$1"
+            shift
+            ;;
+    esac
+done
+
+# Create nmap directory
+mkdir -p nmap
+
+# Perform Nmap scan to identify all open ports
+nmap_cmd="nmap -p- -oN nmap/all_ports $target -vvv"
+if [[ $fast == true ]]; then
+    nmap_cmd+=" --max-rtt-timeout 100ms --max-retries 1"
+fi
+$nmap_cmd
+
+# Extract list of open ports from all_ports file
+ports=$(cat nmap/all_ports | grep "open" | cut -d "/" -f1 | tr "\n" "," | sed 's/.$//')
+echo "Open ports: $ports"
+
+# Perform Nmap scan on open ports to identify services and vulnerabilities
+nmap_cmd="nmap -p$ports -sC -sV $target -oN nmap/services -vvv"
+if [[ $fast == true ]]; then
+    nmap_cmd+=" --max-rtt-timeout 100ms --max-retries 1"
+fi
+$nmap_cmd
