dfe016af6e64dca46cfe4eea18aa51c41b0f42d7
Youness-Harrizi <younessharrizi4@gmail.com>
2023-02-23 21:10:39 +0100

    smbpwn

---
+++ 03_smb_pwn.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enumerate SMB shares
+mkdir smb 
+echo "Enumerating SMB shares..."
+smbclient -L $1 -U ''
+
+# Check for null sessions
+echo "Checking for null sessions..."
+rpcclient -U "" -N $1 -c enumdomusers,srvinfo,querydominfo,enumdomgroups
+
+# Enumerate users and groups
+echo "Enumerating users and groups..."
+enum4linux -a $1 |tee smb/enum4linux.log
+
+# Check for SMB signing vulnerabilities
+echo "Checking for SMB signing vulnerabilities..."
+nmap -Pn -p 445 --script smb-security-mode.nse $1 -oN smb/nmap_smb
+
+
+# Check for SMB vulnerabilities using Nmap
+echo "Checking for SMB vulnerabilities using Nmap..."
+nmap -Pn -p 445 --script smb-vuln* $1 -oN smb/nmap_smbvulns.txt
+
+
+# Check for SMB vulnerabilities using Metasploit
+echo "Checking for SMB vulnerabilities using Metasploit..."
+msfconsole -x "use auxiliary/scanner/smb/smb_version; set RHOSTS $1; run; exit" |tee smb/msfresult.txt
+
+
+
