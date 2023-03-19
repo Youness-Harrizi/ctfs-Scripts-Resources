5abd2170fce29fa2621fc67077a8adff5d4f33a7
Youness-Harrizi <younessharrizi4@gmail.com>
2023-03-19 11:31:29 +0000

    remove recusrive and handle https

--- 02_web_enum.sh
+++ 02_web_enum.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir web 
 dir=$(pwd)
-wordlist="/usr/share/secLists/Discovery/Web-Content/raft-medium-directories.txt"
+wordlist="/usr/share/seclists/Discovery/Web-Content/raft-medium-directories.txt"
 echo "=======start whatweb====="
 whatweb -a 3 $1 |tee web/whatweb.log
 echo "=======start nuclei ====="
 nuclei -u $1 |tee web/nuclei.log
 echo "=======start nikto ====="
 nikto -h $1 |tee web/nikto.log
-echo "=======start dirsearch====="
-feroxbuster -u $1 -w $wordlist -o $dir/feroxbuster.log -t 16 -x html,php,asp,aspx,jsp -C 404,403 
+echo "=======start feroxbuster====="
+feroxbuster -u $1 -w $wordlist -t 16 -x html,php,asp,aspx,jsp -C 404,403  -n -o  web/feroxbuster.log -k
